f0c0620603ac9ac9577b90c55a147eeee1777ad8
Milan Felix Šulc <rkfelix@gmail.com>
2023-06-11 14:53:42 +0200

    Tests: modernize

--- tests/Cases/Unit/DI/FixturesExtensionTest.php
+++ tests/Cases/Unit/DI/FixturesExtensionTest.php
@@ -2,27 +2,23 @@
 
 namespace Tests\Cases\Unit\DI;
 
+use Contributte\Tester\Toolkit;
+use Contributte\Tester\Utils\ContainerBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 use Mockery;
 use Nette\DI\Compiler;
 use Nette\DI\Container;
-use Nette\DI\ContainerLoader;
 use Nettrine\Fixtures\Command\LoadDataFixturesCommand;
 use Nettrine\Fixtures\DI\FixturesExtension;
 use Nettrine\Fixtures\Loader\FixturesLoader;
 use Tester\Assert;
-use Tester\TestCase;
 use Tests\Fixtures\ContainerFixture;
 
 require_once __DIR__ . '/../../../bootstrap.php';
 
-final class FixturesExtensionTest extends TestCase
-{
-
-	public function testLoad(): void
-	{
-		$loader = new ContainerLoader(TMP_DIR, true);
-		$class = $loader->load(function (Compiler $compiler): void {
+Toolkit::test(function (): void {
+	$container = ContainerBuilder::of()
+		->withCompiler(function (Compiler $compiler): void {
 			// Manager registry is needed for console command
 			// It's also provided by nettrine/orm package
 			$compiler->getContainerBuilder()
@@ -40,33 +36,31 @@ final class FixturesExtensionTest extends TestCase
 					],
 				],
 			]);
-		}, __METHOD__ . '1');
+		})
+		->build();
 
-		/** @var Container $container */
-		$container = new $class();
-		$container->addService('managerRegistry', Mockery::mock(ManagerRegistry::class));
+	$container->addService('managerRegistry', Mockery::mock(ManagerRegistry::class));
 
-		/** @var FixturesLoader $loader */
-		$loader = $container->getByType(FixturesLoader::class);
-		Assert::type(FixturesLoader::class, $loader);
+	/** @var FixturesLoader $loader */
+	$loader = $container->getByType(FixturesLoader::class);
+	Assert::type(FixturesLoader::class, $loader);
 
-		// Load fixtures
-		$loader->load();
+	// Load fixtures
+	$loader->load();
 
-		/** @var ContainerFixture $containerFixture */
-		$containerFixture = $loader->getFixture(ContainerFixture::class);
-		Assert::type(ContainerFixture::class, $containerFixture);
-		Assert::type(Container::class, $containerFixture->getContainer());
+	/** @var ContainerFixture $containerFixture */
+	$containerFixture = $loader->getFixture(ContainerFixture::class);
+	Assert::type(ContainerFixture::class, $containerFixture);
+	Assert::type(Container::class, $containerFixture->getContainer());
 
-		/** @var LoadDataFixturesCommand $command */
-		$command = $container->getByType(LoadDataFixturesCommand::class);
-		Assert::type(LoadDataFixturesCommand::class, $command);
-	}
+	/** @var LoadDataFixturesCommand $command */
+	$command = $container->getByType(LoadDataFixturesCommand::class);
+	Assert::type(LoadDataFixturesCommand::class, $command);
+});
 
-	public function testLoadPaths(): void
-	{
-		$loader = new ContainerLoader(TMP_DIR, true);
-		$class = $loader->load(function (Compiler $compiler): void {
+Toolkit::test(function (): void {
+	$container = ContainerBuilder::of()
+		->withCompiler(function (Compiler $compiler): void {
 			// Manager registry is needed for console command
 			// It's also provided by nettrine/orm package
 			$compiler->getContainerBuilder()
@@ -74,24 +68,18 @@ final class FixturesExtensionTest extends TestCase
 				->setType(ManagerRegistry::class);
 
 			$compiler->addExtension('fixtures', new FixturesExtension());
-		}, __FILE__ . '2');
+		})->build();
 
-		/** @var Container $container */
-		$container = new $class();
-		$container->addService('managerRegistry', Mockery::mock(ManagerRegistry::class));
+	$container->addService('managerRegistry', Mockery::mock(ManagerRegistry::class));
 
-		/** @var FixturesLoader $loader */
-		$loader = $container->getByType(FixturesLoader::class);
+	/** @var FixturesLoader $loader */
+	$loader = $container->getByType(FixturesLoader::class);
 
-		// Load fixtures manually with given paths
-		$loader->loadPaths([__DIR__ . '/../../../Fixtures']);
+	// Load fixtures manually with given paths
+	$loader->loadPaths([__DIR__ . '/../../../Fixtures']);
 
-		/** @var ContainerFixture $containerFixture */
-		$containerFixture = $loader->getFixture(ContainerFixture::class);
-		Assert::type(ContainerFixture::class, $containerFixture);
-		Assert::type(Container::class, $containerFixture->getContainer());
-	}
-
-}
-
-(new FixturesExtensionTest())->run();
+	/** @var ContainerFixture $containerFixture */
+	$containerFixture = $loader->getFixture(ContainerFixture::class);
+	Assert::type(ContainerFixture::class, $containerFixture);
+	Assert::type(Container::class, $containerFixture->getContainer());
+});
